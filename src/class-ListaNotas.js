66f581a3ceb7c8b3b5b57dea48697deedaabcecb
Adrian Montano <adrian.montano2011@yahoo.com>
2023-03-06 17:58:46 -0400

    Se mejoró la funcionalidad del botón eliminar/borrar permanentemente una nota de la lista de notas (en una funcion aparte, dentro de la clase ListaNotas)

--- src/class-ListaNotas.js
+++ src/class-ListaNotas.js
@@ -9,33 +9,34 @@ class ListaNotas {
   {
     for(let i=this.arr.length-1;i>=0;i--){
       let nota=this.arr[i];
+
       const li=document.createElement('li');
       li.innerHTML="<b> Titulo: </b>" + nota.titulo;
       if(nota.descripcion!="")
-        li.innerHTML+="<br> <b> Descripcion : </b>" + nota.descripcion;
+      { li.innerHTML+="<br> <b> Descripcion : </b>" + nota.descripcion; }
       li.innerHTML+="<br> <br>";
 
-      //BOTON ELIMINAR NOTA
-      const botonBorrar = document.createElement('button');
+      let botonBorrar = this.eliminarNota(i,lista);
+
+      lista.appendChild(li);//muestra la fila "li" (itera en i)
+      lista.appendChild(botonBorrar); //muestra el botonborrar
+      //li.innerHTML+="<br><br>";
+    }
+    return lista;
+  }
+  eliminarNota(i,lista)
+  {
+    const botonBorrar = document.createElement('button');
       botonBorrar.innerHTML = "BORRAR";
 
-      const LN = this; //crear una instancia y poder trabajar con eventos a continuacion
+      const LN = this; //crear una instancia y poder trabajar con eventos dentro la funcion a continuacion
 
       botonBorrar.addEventListener('click', function() {
         LN.arr.splice(i,1); //elimina 1 (2do parametro) elemento de un array dada una posicion (1er parametro)
         lista.innerHTML = ''; //sobre escribe un espacio en blanco en esa seccion
         LN.mostrarNotas(lista); //vuelve a mostrar la lista despues de haber sido modificada
       });
-
-      lista.appendChild(li);//muestra la fila "li" (itera en i)
-      li.appendChild(botonBorrar); //muestra el botonborrar
-      li.innerHTML+="<br> <br>";
-    }
-    return lista;
-  }
-  eliminarNota()
-  {
-    
+      return botonBorrar;
   }
 }
 
